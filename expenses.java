60b7033b3e83203c3b1776ea0d5847c87ddaf848
Scarlet Galindo <galindoscarlet33@gmail.com>
2025-09-25 23:39:00 -0600

    Create expenses.java

---
+++ expenses.java
@@ -0,0 +1,19 @@
+public class expenses {
+    
+    private String category;
+    private double amount;
+
+    public expenses(String category, double amount) {
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+}
